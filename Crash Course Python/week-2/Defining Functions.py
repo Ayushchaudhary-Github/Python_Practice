35656f87089fa4ac3eb4ae695d8b945f247d31c1
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-09-08 19:11:17 +0530

    github commands

--- Crash Course Python/week-2/Defining Functions.py
+++ Crash Course Python/week-2/Defining Functions.py
@@ -6,7 +6,9 @@ Flesh out the body of the print_seconds function so that it prints the total amo
 minutes, and seconds function parameters. Remember that there are 3600 seconds in an hour and 60 seconds in a minute.
 """
 
+
 def print_seconds(hours, minutes, seconds):
     print(3600*hours+60*minutes+seconds)
 
-print_seconds(1,2,3)
+
+print_seconds(1, 2, 3)
