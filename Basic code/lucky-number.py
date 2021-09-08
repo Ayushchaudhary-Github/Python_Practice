f49c876e086466de2ccfe82fe39e2a18798dd90c
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-09-08 20:03:11 +0530

    find lucky number using funshion

--- Basic code/lucky-number.py
+++ Basic code/lucky-number.py
@@ -1,5 +1,18 @@
+"""
+#Simple way
 name = "Ayush"
 number = len(name)*9
 
 #Ptinting Your name and lucky number 
-print("Hellow "+name+"Your lucky number is "+str(number))
+print("Hello "+name+"Your lucky number is "+str(number))
+
+"""
+
+#Using a function
+
+def lucky_number(name): 
+    number = len(name)*9    
+    print("Hello "+ name +". Your Lucky number is: "+ str(number))
+
+lucky_number("Ayush")
+lucky_number("Yush")
