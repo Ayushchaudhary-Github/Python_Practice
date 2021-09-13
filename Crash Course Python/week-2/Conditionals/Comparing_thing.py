755d965d946c25465ab8f8cecdb236cdb40f80ea
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-09-13 18:49:23 +0530

    Ok this is done

---
+++ Crash Course Python/week-2/Conditionals/Comparing_thing.py
@@ -0,0 +1,24 @@
+"""
+Figure out what's the relationship between the strings 
+"cat" and "Cat" by replacing the plus sign with comparison operators.
+"""
+"""
+print("cat" + "Cat")
+"""
+
+"""
+"cat" equals "Cat"
+
+"cat" is smaller than "Cat"
+
+"cat" is larger than "Cat"
+"""
+
+#Answer
+
+#  code
+#  print("cat" == "Cat")
+
+# false
+
+# "cat" is larger than "Cat" 
