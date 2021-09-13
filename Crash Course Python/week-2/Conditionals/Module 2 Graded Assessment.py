e5638f5ebb824806ef62af9147e9fa821874282d
Yush Chaudhary <Chaudharyayush467@gmail.com>
2021-09-13 21:27:08 +0530

    Fixing the error's

--- Crash Course Python/week-2/Conditionals/Module 2 Graded Assessment.py
+++ Crash Course Python/week-2/Conditionals/Module 2 Graded Assessment.py
@@ -1,5 +1,5 @@
-Question 1- Complete the function by filling in the missing parts. The color_translator function receives the name of a color, then prints its hexadecimal value. Currently, it only supports the three additive primary colors (red, green, blue), so it returns “unknown” for all other colors.
-Answer-
+# Question 1- Complete the function by filling in the missing parts. The color_translator function receives the name of a color, then prints its hexadecimal value. Currently, it only supports the three additive primary colors (red, green, blue), so it returns “unknown” for all other colors.
+# Answer-
 
 def color_translator(color):
 	if color == "red":
@@ -11,6 +11,7 @@ def color_translator(color):
 	else:
 		hex_color = "unknown"
 	return hex_color
+
 print(color_translator("blue")) # Should be #0000ff
 print(color_translator("yellow")) # Should be unknown
 print(color_translator("red")) # Should be #ff0000
@@ -18,15 +19,15 @@ print(color_translator("black")) # Should be unknown
 print(color_translator("green")) # Should be #00ff00
 print(color_translator("")) # Should be unknown
 
-Question 2- What’s the value of this Python expression: “big” > “small”
-Answer- False
+# Question 2- What’s the value of this Python expression: “big” > “small”
+# Answer- False
 
-Question 3- What is the elif keyword used for?
-Answer-
-To handle more than two comparison cases
+# Question 3- What is the elif keyword used for?
+# Answer-
+# To handle more than two comparison cases
 
-Question 4- Students in a class receive their grades as Pass/Fail. Scores of 60 or more (out of 100) mean that the grade is “Pass”. For lower scores, the grade is “Fail”. In addition, scores above 95 (not included) are graded as “Top Score”. Fill in this function so that it returns the proper grade.
-Answer-
+# Question 4- Students in a class receive their grades as Pass/Fail. Scores of 60 or more (out of 100) mean that the grade is “Pass”. For lower scores, the grade is “Fail”. In addition, scores above 95 (not included) are graded as “Top Score”. Fill in this function so that it returns the proper grade.
+# Answer-
 
 def exam_grade(score):
 	if score==100:
@@ -43,22 +44,26 @@ print(exam_grade(95)) # Should be Pass
 print(exam_grade(100)) # Should be Top Score
 print(exam_grade(0)) # Should be Fail
 
-Question 5- What’s the value of this Python expression: 11 % 5?
-Answer- 1
+# Question 5- What’s the value of this Python expression: 11 % 5?
+# Answer- 1
 
-Question 6- Complete the body of the format_name function. This function receives the first_name and last_name parameters and then returns a properly formatted string.
+# Question 6- Complete the body of the format_name function. This function receives the first_name and last_name parameters and then returns a properly formatted string.
 
-Specifically:
+# Specifically:
 
-If both the last_name and the first_name parameters are supplied, the function should return like so:
+# If both the last_name and the first_name parameters are supplied, the function should return like so:
+
 print(format_name("Ella", "Fitzgerald"))
 "Name: Fitzgerald, Ella"
-If only one name parameter is supplied (either the first name or the last name) , the function should return like so:
+
+# If only one name parameter is supplied (either the first name or the last name) , the function should return like so:
+
 print(format_name("Adele", ""))
 "Name: Adele"
 or
 print(format_name(“”, “Einstein”))`
 "Name: Einstein"
+
 Finally, if both names are blank, the function should return the empty string:
 print(format_name("", ""))
 ""
@@ -85,8 +90,8 @@ print(format_name("Voltaire", ""))
 print(format_name("", ""))
 # Should be ""
 
-Question 7- The longest_word function is used to compare 3 words. It should return the word with the most number of characters (and the first in the list when they have the same length). Fill in the blank to make this happen.
-Answer-
+# Question 7- The longest_word function is used to compare 3 words. It should return the word with the most number of characters (and the first in the list when they have the same length). Fill in the blank to make this happen.
+# Answer-
 
 def longest_word(word1, word2, word3):
 	if len(word1) >= len(word2) and len(word1) >= len(word3):
@@ -100,19 +105,22 @@ print(longest_word("chair", "couch", "table"))
 print(longest_word("bed", "bath", "beyond"))
 print(longest_word("laptop", "notebook", "desktop"))
 
-Question 8- What’s the output of this code?
+# Question 8- What’s the output of this code?
 
 def sum(x, y):
 		return(x+y)
 print(sum(sum(1,2), sum(3,4)))
-Answer- 10
 
-Question 9- What’s the value of this Python expression?
+# Answer- 10
+
+# Question 9- What’s the value of this Python expression?
+
 ((10 >= 5*2) and (10 <= 5*2))
-Answer- True
 
-Question 10- The fractional_part function divides the numerator by the denominator, and returns just the fractional part (a number between 0 and 1). Complete the body of the function so that it returns the right number. Note: Since division by 0 produces an error, if the denominator is 0, the function should return 0 instead of attempting the division.
-Answer-
+# Answer- True
+
+# Question 10- The fractional_part function divides the numerator by the denominator, and returns just the fractional part (a number between 0 and 1). Complete the body of the function so that it returns the right number. Note: Since division by 0 produces an error, if the denominator is 0, the function should return 0 instead of attempting the division.
+# Answer-
 
 import math
 def fractional_part(numerator, denominator):
